4912e8b3cf66919279c2b9694e449c2cd6bf3fd0
Simon <simon.lefeuvre@telecom-st-etienne.fr>
2015-10-13 21:28:35 -0400

    Ajout boutons dans MainActivity

--- GeekProject/app/src/main/java/ppm/uqac/com/geekproject/MainActivity.java
+++ GeekProject/app/src/main/java/ppm/uqac/com/geekproject/MainActivity.java
@@ -1,9 +1,11 @@
 package ppm.uqac.com.geekproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +35,24 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void onClickProfil(View v)
+    {
+        Intent intent = new Intent(this,ViewProfileActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickActivites(View v)
+    {
+        Intent intent = new Intent(this,ViewListActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickContent(View v)
+    {
+        Intent intent = new Intent(this,ViewContentActivity.class);
+        startActivity(intent);
     }
 }
