a431588c28c07fa80d1e8e1ddef6ac5edc5a9393
arnaudden <arnaud.denizet@gmail.com>
2015-10-25 07:19:56 +0100

    ajout d'un bouton pour dire qu'on a fait une activité

--- GeekProject/app/src/main/java/ppm/uqac/com/geekproject/ViewListActivity.java
+++ GeekProject/app/src/main/java/ppm/uqac/com/geekproject/ViewListActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -29,6 +30,11 @@ public class ViewListActivity extends AppCompatActivity {
         ListView gaLV = (ListView) findViewById(R.id.ListView01);
         // Adaptation de la ListView avec notre Adapter
         gaLV.setAdapter(gaAdapter);
+    }
+
+    public void onClickActivitiesDone(View v)
+    {
+        Toast.makeText(this, "activite " + v.getId() + "  faite", Toast.LENGTH_SHORT).show();
     }
 
     @Override
