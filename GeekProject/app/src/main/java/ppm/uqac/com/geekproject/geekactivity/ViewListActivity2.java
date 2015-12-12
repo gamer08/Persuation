6fdbab17c255c3e839ee7746be496aba51a8766e
arnaudden <arnaud.denizet@gmail.com>
2015-12-12 18:27:10 +0100

    vérification si l'application Appy Geek est installé sur son appareil

--- GeekProject/app/src/main/java/ppm/uqac/com/geekproject/geekactivity/ViewListActivity2.java
+++ GeekProject/app/src/main/java/ppm/uqac/com/geekproject/geekactivity/ViewListActivity2.java
@@ -5,6 +5,8 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -27,6 +29,8 @@ import com.facebook.CallbackManager;
 import com.facebook.share.model.ShareLinkContent;
 import com.facebook.share.widget.ShareDialog;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import ppm.uqac.com.geekproject.Database.GADatabase;
 import ppm.uqac.com.geekproject.R;
@@ -52,6 +56,9 @@ public class ViewListActivity2 extends AppCompatActivity implements NavigationVi
 
     Boolean _firsttime=false;
 
+    List<ApplicationInfo> _installedApps = null;
+    PackageManager pm;
+
     // Variables pour le partage sur facebook
     CallbackManager callbackManager;
     ShareDialog shareDialog;
@@ -63,6 +70,16 @@ public class ViewListActivity2 extends AppCompatActivity implements NavigationVi
         setContentView(R.layout.activity_view_list2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Get Application installed on devices
+
+        pm = getPackageManager();
+        _installedApps = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        for(int i =0; i<_installedApps.size(); i++)
+        {
+            System.out.println(_installedApps.get(i).packageName);
+        }
+
 
         Intent intent = getIntent();
         if (intent != null)
@@ -244,9 +261,25 @@ public class ViewListActivity2 extends AppCompatActivity implements NavigationVi
 
     public void onClickActivitiesDone(View v)
     {
+        boolean isApp = false;
         Fragment currentFragment = this.getFragmentManager().findFragmentById(R.id.activities_frame);
         GA activity = gaAdapter.getItem(fragment_ga.get_ContentListView().getPositionForView(v));
         System.out.println("ViewListActivity2 : Appuye bouton " + activity.get_name());
+        if(activity.get_name().equals("Installer l application AppyGeek"))
+        {
+            isApp = searchApps("com.mobilesrepublic.appygeek");
+            System.out.println(isApp);
+            if(!isApp)
+            {
+                Toast.makeText(this, "Nous n'avons pas trouvé l'application Appy Geek sur votre apparaeil", Toast.LENGTH_SHORT).show();
+                gaList.clear();
+                gaList = gadb.getActivitiesDoing(_profile.getLevel());
+                gaAdapter.updateListView(gaList);
+                fragment_ga.set_gadapter(gaAdapter);
+                fm.beginTransaction().detach(currentFragment).attach(currentFragment).commit();
+                return;
+            }
+        }
         gadb.updateActivity(activity);
         gaList.clear();
         gaList = gadb.getActivitiesDoing(_profile.getLevel());
@@ -321,4 +354,25 @@ public class ViewListActivity2 extends AppCompatActivity implements NavigationVi
         fragment_ga.set_gadapter(gaAdapter);
         fm.beginTransaction().detach(currentFragment).attach(currentFragment).commit();
     }
+
+    public boolean searchApps(String name)
+    {
+        int i =0;
+        boolean isApp = false;
+        if(!_installedApps.isEmpty())
+            _installedApps.clear();
+        _installedApps = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        System.out.println(_installedApps.size());
+        while( i<(_installedApps.size()-1) && !isApp)
+        {
+            if (_installedApps.get(i).packageName.equals(name)) {
+                isApp = true;
+            } else {
+                isApp = false;
+            }
+            i++;
+        }
+        return isApp;
+    }
 }
+
